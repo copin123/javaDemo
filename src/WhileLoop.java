b5eb502448a9516009d2c807ade7c54737348e2d
copin123@gmail.com <copin123@gmail.com>
2017-06-15 21:46:33 -0500

    while loop

---
+++ src/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+  public static void main(String[] args) {
+    System.out.println("This will demo a while loop");
+
+    int stopVal = 0;
+    while (stopVal < 10){
+      System.out.println("the loop value is "+stopVal);
+      stopVal++;
+    }
+  }
+}
